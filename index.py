991928f219b5366fb2c88c62b8e9bc266de2bd9c
Vrinda Bisani <vrindabisani@gmail.com>
2023-06-22 15:07:13 -0700

    ui

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import streamlit as st
 import datetime
 
 st.title('Student Financial Planner')
+# data = yaml.safe_load(open("countries+states+cities.yml","r"))
 
 with st.form("my_form"):
    dob = st.date_input("DOB", datetime.date.today(), max_value=datetime.date.today(), min_value = datetime.date(1950,1,1))
@@ -16,17 +17,32 @@ with st.form("my_form"):
    speciality = st.text_input('What specialty are you expecting to enroll?')
    term = st.selectbox('Which term are you expecting to enroll in?',('Fall', 'Spring', 'Winter'))
    year = st.text_input('Enrollment year')
-   st.text("Which of the following topics do you need help with?")
-   is_student_loan = st.checkbox("Student Loan")
-   is_financial_aid = st.checkbox("Financial Aid")
-   is_scholarship_grant = st.checkbox("Scolarship and Grant")
-   is_financial_budget = st.checkbox("Financial Budget")
-   is_bank_account = st.checkbox("Opening a Bank Account")
-   is_creditcard = st.checkbox("Credit Card Recommendation")
-
-   # Every form must have a submit button.
    submitted = st.form_submit_button("Submit")
-   if submitted:
-       st.write("hello")
+
+st.text("Which of the following topics do you need help with?")
+is_student_loan = st.checkbox("Student Loan")
+is_financial_aid = st.checkbox("Financial Aid")
+is_scholarship_grant = st.checkbox("Scolarship and Grant")
+is_financial_budget = st.checkbox("Financial Budget")
+if is_financial_budget:
+    st.slider('What is you budget?', 0, 10000, 25)
+is_bank_account = st.checkbox("Opening a Bank Account")
+is_creditcard = st.checkbox("Credit Card Recommendation")
+get_info = st.button("OK")
+
+if get_info:
+    tab = []
+    tab_data = {"Student Loan" : is_student_loan, 
+                "Financial Aid" : is_financial_aid,
+                "Scholarship & Grants" : is_scholarship_grant,
+                "Financial Budget" : is_financial_budget,
+                "Bank" : is_bank_account,
+                "Credit Card" : is_creditcard}
+
+    for key, value in tab_data.items():
+        if value:
+            tab.append(key)
+    tabs = st.tabs(tab)
+       
 
 
