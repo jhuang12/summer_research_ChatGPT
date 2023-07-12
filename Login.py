5fa4eeccb0d84928d4005f19ecf6ca847f71a959
Vrinda Bisani <vrindabisani@gmail.com>
2023-07-12 11:46:32 -0700

    Login, signup and expense tracker fetaure impl

---
+++ Login.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import redis
+import datetime
+
+st.set_page_config(page_title="Login")
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+st.title('Login / Signup')
+tab1, tab2 = st.tabs(["Login", "SignUp"])
+
+with tab1: 
+    with st.form("login_form"):
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+        submit_button = st.form_submit_button("Login")
+    
+    if submit_button:
+        stored_password = r.hget(username, "password")
+        if stored_password == password:
+            st.success("Login successful!")
+            st.session_state['username'] = username
+            choice = "Expense Tracker"
+        else:
+            st.error("Invalid username or password.")
+
+with tab2:
+    st.subheader("SignUp Section")
+    with st.form("my_form"):
+        username = st.text_input("User Name")
+        password = st.text_input("Password",type='password')
+        email = st.text_input("Email")
+        dob = st.date_input("DOB", datetime.date.today(), max_value=datetime.date.today(), min_value = datetime.date(1950,1,1))
+        gender = st.radio("Gender", ('Male', 'Female', 'Other'))
+        city = st.text_input('City')
+        submitted = st.form_submit_button("Submit")
+    if submitted:
+        if r.exists(username):
+            st.error("Username already exists. Please choose a different username.")
+        else:
+            user_data = {
+                "password": password,
+                "city": city,
+                "dob": dob.strftime("%Y-%m-%d"),
+                "gender": gender,
+                "email": email,
+            }
+            r.hset(username, mapping=user_data)
+            st.success("Signup successful. You can now login.")
+
+
---
+++ pages/2_Expense_Tracker.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import redis
+import pandas as pd
+import datetime
+import json
+from streamlit_extras.switch_page_button import switch_page
+
+st.set_page_config(page_title="Expense Tracker")
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+st.title('Expense Tracker')
+
+if 'username' not in st.session_state:
+    st.text("Please Login")
+    if st.button("Login"):
+        switch_page("Login")
+else:
+    with st.form("expense_tracker"):
+        category = st.selectbox("Category", (
+            "Groceries",
+            "Dining/Restaurants",
+            "Transportation",
+            "Utilities",
+            "Rent/Mortgage",
+            "Insurance",
+            "Entertainment",
+            "Travel/Vacation",
+            "Clothing/Apparel",
+            "Personal Care",
+            "Education",
+            "Healthcare/Medical expenses",
+            "Home Maintenance/Repairs",
+            "Subscriptions",
+            "Gifts/Donations",
+            "Miscellaneous/Other"
+        ))
+        description = st.text_input("Note")
+        expense_date = st.date_input("Date", datetime.date.today(), max_value=datetime.date.today(), min_value = datetime.date(1950,1,1))
+        amount = st.number_input('Amount')
+        submit_button = st.form_submit_button("Add")
+
+    if submit_button:
+            expense_id = r.incr(f"{st.session_state['username']}:counter")
+            expense_data = {
+                "Category": category,
+                "Description": description,
+                "Date" : expense_date.strftime('%y/%m/%d'),
+                "Amount": amount,
+            }
+            r.hset(f"{st.session_state['username']}:expenses", expense_id, json.dumps(expense_data))
+            st.success("Added")
+    
+    expense_entries = r.hgetall(f"{st.session_state['username']}:expenses")
+    if expense_entries:
+        st.subheader("Expense Entries")
+
+        expense_data = []
+        for expense_id, expense_entry in expense_entries.items():
+            expense_entry = json.loads(expense_entry)
+            expense_data.append(expense_entry)
+        df = pd.DataFrame(expense_data)
+        st.dataframe(df)
+
+
